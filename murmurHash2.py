619608df663881fb8394db928af3b48205bc79c4
Maxim <toper.one111@gmail.com>
2023-05-08 18:55:52 +0300

    Define main constants

--- murmurHash2.py
+++ murmurHash2.py
@@ -3,4 +3,11 @@ def ensureInt4Bytes(input: int):
 
 
 def murmur2(input: bytes):
-    pass
+    # m is used in the MurmurHash algorithm as a magic constant multiplier
+    # value of 0x5bd1e995 was chosen because it is
+    # a prime number with good bit diffusion properties
+    m = 0x5bd1e995
+
+    # number of bits to be shifted
+    r = 24
+    seed = 0
